63ea9b1b036e76dd6c602ddee3a94832a4b40df8
elise-love <elisadiskg5@gmail.com>
2026-01-27 17:00:48 +0800

    wrote tailwiind confiig

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import colors from "tailwindcss/colors";
 
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "../app/templates/**/*.html"
+    ],
+    theme: {
+        colors: {
+            gray: colors.coolGray,
+            blue: colors.lightBlue,
+            red: colors.rose,
+            pink: colors.fuchsia,
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            },
+        },
+    },
+    plugins: [],
+};
