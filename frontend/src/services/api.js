037f94945a36e43de36cbeef80cc77809c848538
elise-love <elisadiskg5@gmail.com>
2026-01-28 16:20:59 +0800

    fixed api.js typo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,17 +12,21 @@ const api = axios.create({
 export const getScale = async (scaleId) => {
     const response = await api.get(`/scales/${scaleId}`);
     return response.data;
+};
 
 //submit responses
-export const submitScaleResponses = async (scaleId, answers, userID =null) => {
-    const response = await api.post('/scales/${scaledID}/responses', {
+export const submitScaleResponses = async (scaleId, answers, userId =null) => {
+    const response = await api.post(`/scales/${scaleId}/responses`, {
         answers,
         user_id: userId,
-    })
+    });
     return response.data;
-    };
+};
 
 //GET history
-export const getUserHistory = a
+export const getUserHistory = async (userId) => {
+  const response = await api.get(`/users/${userId}/history`);
+  return response.data;
+};
 
-
+export default api;
