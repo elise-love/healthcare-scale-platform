4fd2eedc3978a55f669363b5f49a2ae9d43d53e3
elise-love <elisadiskg5@gmail.com>
2026-02-03 14:00:50 +0800

    Wrote ResultPage.jsx

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -1,14 +1,24 @@
-import React form 'react';
-import(useLocation) from 'react-router-dom';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import ResultDisplay from '../components/ResultDisplay/ResultDisplay';
 import ErrorMessage from '../components/common/ErrorMessage';
 import './ResultPage.css';
 
 const ResultPage = () => {
     const Location = useLocation();
+    const result = location.state?.result;
 
+    if (!result) {
+        return <ErrorMessage message="無法取得測驗結果 ><" />;
+    }
 
+    return (
+        <div className="resultPageConstainer">
+            <ResultDisplay result={result} />
+        </div>
+    );
+};
 
-}
+export default ResultPage;
 
 
