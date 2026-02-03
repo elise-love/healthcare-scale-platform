7351ca278a62bff91f6842d95112eb91543d70d0
elise-love <elisadiskg5@gmail.com>
2026-02-03 13:18:01 +0800

    .

---
+++ frontend/src/pages/ResultPage.jsx
@@ -0,0 +1,14 @@
+import React form 'react';
+import(useLocation) from 'react-router-dom';
+import ResultDisplay from '../components/ResultDisplay/ResultDisplay';
+import ErrorMessage from '../components/common/ErrorMessage';
+import './ResultPage.css';
+
+const ResultPage = () => {
+    const Location = useLocation();
+
+
+
+}
+
+
